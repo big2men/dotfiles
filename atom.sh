05880ef106c52122eaa222b61468bc54ae0014bc
Shun Hotta <stars.big2men@gmail.com>
2015-12-07 11:33:25 +0900

    Add atom package install script

---
+++ atom.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Minimap
+apm install minimap
+apm install minimap-autohide
+apm install minimap-bookmarks
+apm install minimap-git-diff
+apm install minimap-highlight-selected
+
+# Rails
+apm install autocomplete-paths
+apm install rails-transporter
+apm install language-haml
+apm install slim-language
+
+# Utility
+apm install sort-lines
+apm install highlight-selected
+
+# Project
+apm install project-manager
